c28e9d34c2991e21bbf3278dae2b06b0dc0977ae
kei-toriki <sakanayama1954@icloud.com>
2021-03-30 19:23:51 +0900

    バリデーション修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to "#"
     else
@@ -19,6 +19,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:item_name, :explanation, :category_id, :status_id, :burden_id, :area_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :explanation, :category_id, :status_id, :burden_id, :area_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
