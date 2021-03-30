554e9108eb49cd932ef62a6293c06ab96f67f0d3
kei-toriki <sakanayama1954@icloud.com>
2021-03-30 20:19:18 +0900

    ユーザー制限修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user! , only: :new
+  before_action :authenticate_user! , only: [:new, :create]
 
   def index
   end
@@ -11,7 +11,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to "#"
+      redirect_to root_path
     else
       render :new
     end
