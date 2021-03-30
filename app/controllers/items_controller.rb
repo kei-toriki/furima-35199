f081d6ac26677374cc250d294754e0f6484862d5
kei-toriki <sakanayama1954@icloud.com>
2021-03-30 13:39:57 +0900

    商品の保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :explanation, :category_id, :status_id, :burden_id, :area_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
 end
