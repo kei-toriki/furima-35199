3df8b154427bdc7afc993311ed0716f923f1a06b
kei-toriki <sakanayama1954@icloud.com>
2021-03-30 15:30:41 +0900

    ログアウト時商品出品不可に実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user! , only: :new
+
   def index
   end
 
@@ -8,6 +10,11 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.create(item_params)
+    if @item.save
+      redirect_to "#"
+    else
+      render :new
+    end
   end
 
   private
