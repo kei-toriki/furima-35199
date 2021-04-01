b0a946e286f0df0e1407578fc16aafa1b51f675b
kei-toriki <sakanayama1954@icloud.com>
2021-04-01 13:08:01 +0900

    商品情報編集機能再修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit]
-  before_action :move_to_index, only: [:edit, :edit]
-  before_action :set_item, only: [:edit, :show]
-
+  before_action :set_item, only: [:edit, :show, :update, :move_to_index]
+  before_action :move_to_index, only: [:edit, :update]
+  
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -27,7 +27,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -41,15 +40,16 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :explanation, :category_id, :status_id, :burden_id, :area_id, :shipping_day_id,
                                  :price, :image).merge(user_id: current_user.id)
   end
+  
+  def move_to_index
+      unless @item.user.id == current_user.id
+      redirect_to action: :index
+    end
+  end
 
   def set_item
     @item = Item.find(params[:id])
   end
 
-  def move_to_index
-    @item = Item.find(params[:id])
-      unless @item.user.id == current_user.id
-      redirect_to action: :index
-    end
-  end
+  
 end
