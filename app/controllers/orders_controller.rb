c8d3ed022b6ff8e95abb58ef2a9ecfb17e166387
kei-toriki <sakanayama1954@icloud.com>
2021-04-02 18:22:14 +0900

    クレジットカード決済処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,20 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @item = Item.find(params[:item_id])
     @order_delivery = OrderDelivery.new
     @order = Order.new
+    if current_user.id == @item.user.id
+      redirect_to root_path
+    end
   end
 
   def create
     @order_delivery = OrderDelivery.new(order_params)
-    binding.pry
+    @item = Item.find(params[:item_id])
     if @order_delivery.valid?
+      pay_item
       @order_delivery.save
       redirect_to root_path
     else
@@ -20,7 +26,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_delivery).permit(:postal_code, :area_id, :municipalities, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_delivery).permit(:postal_code, :area_id, :municipalities, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,  
+        card: order_params[:token],    
+        currency: 'jpy'                 
+      )
+    end
 
 end
--- app/models/order_delivery.rb
+++ app/models/order_delivery.rb
@@ -1,6 +1,6 @@
 class OrderDelivery
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :area_id, :municipalities, :address, :building, :phone_number, :order_id
+  attr_accessor :user_id, :item_id, :postal_code, :area_id, :municipalities, :address, :building, :phone_number, :order_id, :token
 
   with_options presence: true do
     validates :user_id
@@ -10,10 +10,10 @@ class OrderDelivery
     validates :address
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :phone_number, format: { with: /\A\d{11}\z/ }
+    validates :token
   end
 
   def save
-    binding.pry
     order = Order.create(user_id: user_id, item_id: item_id)
     Delivery.create(postal_code: postal_code, area_id: area_id, municipalities: municipalities, address: address, building: building, phone_number: phone_number, order_id: order.id)
   end
